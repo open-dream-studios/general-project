3afba88e65f44672ca8fc4693a18bbdad5e3d4d3
JosephGoff <joeygoff13@gmail.com>
2025-08-23 20:14:51 -0400

    4

--- server/connection/firebaseAdmin.js
+++ server/connection/firebaseAdmin.js
@@ -1,5 +1,9 @@
 import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json" assert { type: "json" };
+// import serviceAccount from "../serviceAccountKey.json" assert { type: "json" };
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64").toString("utf-8")
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
