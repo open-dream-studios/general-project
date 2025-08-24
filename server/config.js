679e26fabfcc2e341a8ff8179d1d7d024fd9d351
JosephGoff <joeygoff13@gmail.com>
2025-08-24 18:19:11 -0400

    6

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const local_frontend = false;
 
-const local_backend = true
+const local_backend = false
 
 export const BACKEND_URL = local_backend? "http://localhost:8080" : "https://general-project-production.up.railway.app"
 export const FRONTEND_URL = local_frontend ? "http://localhost:3000" : "https://tannyspaacquisitions.shop"
